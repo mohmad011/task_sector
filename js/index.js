19129da05ad5b4bdb49f897cb26ba6291e222fe4
Mohmad012 <mohmadailsia@gmail.com>
2021-07-30 21:22:36 -0400

    finish task

---
+++ js/index.js
@@ -0,0 +1,29 @@
+import Highway from '@dogstudio/highway'
+import {Fade , Fade2 , Fade3} from './transitionD'
+
+
+const H = new Highway.Core({
+	transitions: {
+		home: Fade,
+		search:Fade2,
+		products: Fade3,
+		end:Fade2
+	}
+})
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
